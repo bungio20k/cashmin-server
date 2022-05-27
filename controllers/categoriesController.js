86d169e780d716e5c9ea8e792011e58da370aceb
Toan <trantoan14022001@gmail.com>
2022-05-27 15:33:05 +0700

    return _id for ADD operator

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -3,55 +3,56 @@ import { user } from "../models/userModel.js";
 const add = async (req, res) => {
     try {
         const userData = await user.findOne({ username: req.username });
-        if (!userData) { return res.status(404).send({"error": "user not found"}); }
+        if (!userData) { return res.status(404).send({ "error": "user not found" }); }
 
         const newCategory = req.body;
         userData.categories.push(newCategory);
-    
+
         userData
             .save()
-            .then(() => {
-                return res.status(200).send(`${req.body.name} - ${req.body.icon} added sucessfully`);
+            .then((saved) => {
+                return res.status(200).send({ _id: saved._id.toString() });
             })
             .catch((err) => {
                 return res.status(500).send(err.message);
             })
     }
     catch (err) {
-        return res.status(500).send({"error": err.message});
+        return res.status(500).send({ "error": err.message });
     }
 }
 
 const update = async (req, res) => {
     try {
         const userData = await user.findOne({ username: req.username });
-        if (!userData) { return res.status(404).send({"error": "user not found"}); }
+        if (!userData) { return res.status(404).send({ "error": "user not found" }); }
 
-        const {_id, ...categoryInfo} = req.body;
+        const { _id, name, icon } = req.body;
         const index = userData.categories.findIndex(e => e._id == _id);
-        userData.categories[index] = categoryInfo;
+        userData.categories[index].name = name;
+        userData.categories[index].icon = icon;
         
         userData
             .save()
             .then(() => {
-                return res.status(200).send(`${categoryInfo.name} - ${categoryInfo.icon} update sucessfully`);
+                return res.status(200).send(`${name} - ${icon} update sucessfully`);
             })
             .catch((err) => {
                 return res.status(500).send(err.message);
             })
     }
     catch (err) {
-        return res.status(500).send({"error": err.message});
+        return res.status(500).send({ "error": err.message });
     }
 }
 
 const remove = async (req, res) => {
     try {
         const userData = await user.findOne({ username: req.username });
-        if (!userData) { return res.status(404).send({"error": "user not found"}); }
+        if (!userData) { return res.status(404).send({ "error": "user not found" }); }
 
         userData.categories = userData.categories.filter(e => e._id != req.body._id)
-        
+
         userData
             .save()
             .then(() => {
@@ -62,11 +63,11 @@ const remove = async (req, res) => {
             })
     }
     catch (err) {
-        return res.status(500).send({"error": err.message});
+        return res.status(500).send({ "error": err.message });
     }
 }
 
-export { 
+export {
     add,
     update,
     remove
